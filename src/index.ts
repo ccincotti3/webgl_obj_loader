b96c8754795144a1970e2a760dc9be87bb9745de
Carmen Cincotti <cincotti@tuta.io>
2020-11-27 14:43:26 -0500

    add index buffer

--- src/index.ts
+++ src/index.ts
@@ -25,21 +25,34 @@ const index = function () {
     0.25,
     0,
     0,
+    0,
+    -0.5 * Math.sqrt(3),
+    0,
   ]);
+
+  const indices = new Uint16Array([0, 1, 2, 2, 3, 0]);
+
   const positionLocation = gl.getAttribLocation(program, "a_position");
 
   const buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
+
   gl.enableVertexAttribArray(positionLocation);
   gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
 
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  const indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+  // UNSIGNED_SHORT, not UNSIGNED_INT
+  gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
 
   // Cleanup
   gl.useProgram(null);
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
   gl.disableVertexAttribArray(positionLocation);
 };
 
